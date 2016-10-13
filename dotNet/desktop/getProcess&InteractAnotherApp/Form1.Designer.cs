3280e18edf895413238f9c13541f15ed1c1739ef
joseph-jose <josephson.sw@gmail.com>
2016-10-13 14:19:02 +1300

    Add files via upload

---
+++ dotNet/desktop/getProcess&InteractAnotherApp/Form1.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace getProcess
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.button10 = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(322, 48);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "getP";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(373, 117);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(339, 365);
+            this.textBox1.TabIndex = 2;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(27, 117);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(293, 368);
+            this.listBox1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(480, 54);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(152, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Projectwise TransferText";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(296, 501);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "getLoc";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(392, 501);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(366, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(27, 501);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(931, 384);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(128, 23);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Projectwise Copy/Paste";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(732, 386);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(187, 20);
+            this.textBox3.TabIndex = 9;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(732, 412);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(187, 20);
+            this.textBox4.TabIndex = 10;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(960, 285);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "PW-Open Meter";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(854, 283);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 12;
+            this.textBox5.Text = "51296";
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(961, 244);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 13;
+            this.button7.Text = "getPW";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(961, 322);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 14;
+            this.button8.Text = "Send Load";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(961, 462);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 23);
+            this.button9.TabIndex = 15;
+            this.button9.Text = "xtPW";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(854, 325);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 16;
+            this.textBox6.Text = "^{L}";
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(882, 124);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(75, 23);
+            this.button10.TabIndex = 17;
+            this.button10.Text = "culminate";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 541);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1077, 22);
+            this.statusStrip1.TabIndex = 18;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(732, 158);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(327, 53);
+            this.panel1.TabIndex = 19;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1077, 563);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ dotNet/desktop/getProcess&InteractAnotherApp/Form1.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Management;
+
+//namespace SysWin32
+//{
+//    [DllImport("user32.dll")]
+//    static extern int SetForegroundWindow(IntPtr point);
+//}
+//namespace SysWin32
+//{
+//    [DllImport("user32.dll", EntryPoint = "MessageBox")]
+//    Int32 static MessageBox(Int32 hWnd, String* lpText, String* lpCaption,
+//                      UInt32 uType);
+//}
+
+namespace getProcess
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("msvcrt.dll")]
+        public static extern int puts(string c);
+        [DllImport("msvcrt.dll")]
+        internal static extern int _flushall();
+        [DllImport("user32.dll")]
+        static extern int SetForegroundWindow(IntPtr point);
+
+        public Process vPWProc;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string vLstMsg = "";
+            textBox1.Text = ""; 
+            listBox1.Items.Clear();
+            Process[] getProcs = Process.GetProcesses();
+            MessageBox.Show ( getProcs.Count().ToString());
+            foreach (Process vProc in getProcs)
+            {
+                //vLstMsg = vProc.ProcessName + "," +  vProc.StartTime.ToString("hh:mmm:ss")  ;
+                vLstMsg = vProc.ProcessName ;
+                listBox1.Items.Add(vLstMsg);
+                textBox1.Text = textBox1.Text + vLstMsg; 
+            }
+        }
+
+
+        private void sendMsg(Process inProc)
+        {
+            IntPtr vPointer = inProc.MainWindowHandle;
+            SetForegroundWindow(vPointer);
+        }
+
+    private void button2_Click(object sender, EventArgs e)
+        {
+            string vLstMsg = "";            
+
+            Process[] vProcesses = Process.GetProcessesByName("IMSV732");
+            foreach (Process vProc in vProcesses)
+            {
+                //vLstMsg = vProc.ProcessName + "," +  vProc.StartTime.ToString("hh:mmm:ss")  ;
+                vLstMsg = vProc.ProcessName;
+                listBox1.Items.Add(vLstMsg);
+                textBox1.Text = textBox1.Text + vLstMsg;
+                sendMsg(vProc);
+                SendKeys.SendWait("%(D)");
+                SendKeys.SendWait("JOSEPH");
+                SendKeys.SendWait("{TAB 4}");
+                SendKeys.SendWait("JOSEPH");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string[] c1;
+            string vStrVal = listBox1.Items[listBox1.SelectedIndex].ToString();
+            foreach (Process vProc in Process.GetProcessesByName(vStrVal) )
+            {
+                try
+                {
+                    using (ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT CommandLine FROM Win32_Process WHERE ProcessId = " + vProc.Id))
+                    {
+                        foreach (ManagementObject mgmtObj in searcher.Get() )
+                        {
+                            c1 = mgmtObj["CommandLine"].ToString().Split(',');//.Split(""");
+                            textBox2.Text = c1[c1.Length - 1];
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+                //System.Threading.Thread.Sleep(100000);
+            }
+            //Dim cl() As String
+            //For Each p As Process In Process.GetProcessesByName("notepad")
+            //    Try
+            //        Using searcher As New ManagementObjectSearcher("SELECT CommandLine FROM Win32_Process WHERE ProcessId = " & p.Id)
+            //            For Each mgmtObj As ManagementObject In searcher.Get()
+            //                cl = mgmtObj.Item("CommandLine").ToString().Split("""")
+            //                Console.WriteLine(cl(cl.Length - 1))
+            //            Next
+            //        End Using
+            //    Catch ex As Win32Exception
+            //        'handle error
+            //    End Try
+            //Next
+            //System.Threading.Thread.Sleep(1000000)
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+            //listBox1.Items[listBox1.SelectedIndex]. 
+        }
+
+        private void getHansenHandle()
+        {
+            Process[] vProcesses = Process.GetProcessesByName("IMSV732");
+            vPWProc = vProcesses[0];
+        }
+
+        private void closeWindowHandle()
+        {
+            vPWProc = null;
+        }
+
+        private void openLoadForm()
+        {
+            if (!(vPWProc == null))
+            {
+                sendMsg(vPWProc);
+                SendKeys.SendWait("{TAB 1}");
+                SendKeys.SendWait("^{l}");
+                System.Threading.Thread.Sleep(3000);
+                SendKeys.SendWait("^{s}");
+                //SendKeys.SendWait(textBox6.Text );
+
+                //SendKeys.SendWait("^{O}");
+                //Application.DoEvents();
+                //SendKeys.SendWait("^{S}");
+
+
+                //SendKeys.SendWait("{HOME}");
+                //SendKeys.SendWait("+{END}");
+                //SendKeys.SendWait("^{INS}");
+
+                //textBox3.Text = Clipboard.GetText();
+
+                //SendKeys.SendWait("%(T)");
+                //SendKeys.SendWait("{TAB 1}");
+                //SendKeys.SendWait("{HOME}");
+                //SendKeys.SendWait("+{END}");
+                //SendKeys.SendWait("^{INS}");
+                //textBox4.Text = Clipboard.GetText();            }
+            }
+        }
+        private void copyContends()
+        {
+            if (!(vPWProc == null))
+            {
+                sendMsg(vPWProc);
+                //SendKeys.SendWait("{TAB 1}");
+                SendKeys.SendWait("{HOME}");
+                SendKeys.SendWait("+{END}");
+                SendKeys.SendWait("^{INS}");
+
+                System.Threading.Thread.Sleep(3000);
+
+                textBox3.Text = Clipboard.GetText();
+
+                System.Threading.Thread.Sleep(3000);
+
+                SendKeys.SendWait("%(T)");
+                SendKeys.SendWait("{TAB 1}");
+                SendKeys.SendWait("{HOME}");
+                SendKeys.SendWait("+{END}");
+                SendKeys.SendWait("^{INS}");
+                textBox4.Text = Clipboard.GetText();
+                System.Threading.Thread.Sleep(3000);
+                SendKeys.SendWait("^{F4}");
+            }
+        }
+
+        private void openAssetWindow()
+        {
+            if (!(vPWProc == null))
+            {
+                sendMsg(vPWProc);
+                //SendKeys.SendWait("{TAB 1}");
+                SendKeys.SendWait("%{A}");
+                SendKeys.SendWait("{DOWN 3}");
+                SendKeys.SendWait("{RIGHT 1}");
+                Application.DoEvents();
+                SendKeys.SendWait("{DOWN 3}");
+                SendKeys.SendWait("{ENTER 1}");
+                Application.DoEvents();
+                SendKeys.SendWait("^{O}");
+                SendKeys.SendWait("%{G}");
+                Application.DoEvents();
+                SendKeys.SendWait(textBox5.Text);
+                Application.DoEvents();
+                System.Threading.Thread.Sleep(3000);
+                SendKeys.SendWait("{TAB 1}");
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            copyContends();
+
+            //string vLstMsg = "";
+            //Process[] vProcesses = Process.GetProcessesByName("IMSV732");
+            //foreach (Process vProc in vProcesses)
+            //{
+
+
+            //    sendMsg(vProc);
+            //    //SendKeys.SendWait("{TAB 1}");
+            //    SendKeys.SendWait("{HOME}");
+            //    SendKeys.SendWait("+{END}");
+            //    SendKeys.SendWait("^{INS}");
+
+            //    textBox3.Text =  Clipboard.GetText();
+
+            //    SendKeys.SendWait("%(T)");
+            //    SendKeys.SendWait("{TAB 1}");
+            //    SendKeys.SendWait("{HOME}");
+            //    SendKeys.SendWait("+{END}");
+            //    SendKeys.SendWait("^{INS}");
+            //    textBox4.Text = Clipboard.GetText();
+
+            //}
+        }
+        private void button6_Click(object sender, EventArgs e)
+        {
+            //string vLstMsg = "";
+
+            //Process[] vProcesses = Process.GetProcessesByName("IMSV732");
+            //foreach (Process vProc in vProcesses)
+            openAssetWindow();
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            getHansenHandle();
+        }
+
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            openLoadForm();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            closeWindowHandle();
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Getting handler....";
+            button7_Click(this, null);
+            label1.Text = "Opening hansen window....";
+            button6_Click(this, null);
+            label1.Text = "Finding asset....";
+            button8_Click(this, null);
+            label1.Text = "Loading asset....";
+            button5_Click(this, null);
+            label1.Text = "Copying attributes....";
+            button9_Click(this, null);
+            label1.Text = "Completed";
+        }
+    }
+}
