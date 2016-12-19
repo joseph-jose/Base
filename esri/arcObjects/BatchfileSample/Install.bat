c06e2370708ebfc32666b6e022cf4985904c480e
joseph-jose <josephson.sw@gmail.com>
2016-12-20 08:25:44 +1300

    Add files via upload

---
+++ esri/arcObjects/BatchfileSample/Install.bat
@@ -0,0 +1,4 @@
+Rem: Install dlls
+"C:\Program Files (x86)\Common Files\ArcGIS\bin\ESRIRegAsm.exe" "C:\Program Files (x86)\ArcGIS\Desktop10.3\bin\Addins\WSLTools\WCTools.dll"
+echo installed
+pause
---
+++ esri/arcObjects/BatchfileSample/UnInstall.bat
@@ -0,0 +1,4 @@
+Rem: UnInstall dlls
+"C:\Program Files (x86)\Common Files\ArcGIS\bin\ESRIRegAsm.exe" "C:\Program Files (x86)\ArcGIS\Desktop10.3\bin\Addins\WSLTools\WCTools.dll" /u
+echo Uninstalled
+pause
