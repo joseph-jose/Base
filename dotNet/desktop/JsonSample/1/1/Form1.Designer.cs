b06faa0164b75e32225f3ff8a856a4cef0e2d024
joseph-jose <josephson.sw@gmail.com>
2017-01-26 08:39:57 +1300

    Add files via upload

---
+++ dotNet/desktop/JsonSample/1/1/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace _1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column10 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column11 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column12 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column13 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(177, 58);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(473, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = resources.GetString("textBox1.Text");
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(238, 94);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(177, 140);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(426, 206);
+            this.textBox2.TabIndex = 2;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7,
+            this.Column8,
+            this.Column9,
+            this.Column10,
+            this.Column11,
+            this.Column12,
+            this.Column13});
+            this.dataGridView1.Location = new System.Drawing.Point(158, 363);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(603, 150);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Column1";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Column2";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Column3";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Column4";
+            this.Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Column5";
+            this.Column5.Name = "Column5";
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Column6";
+            this.Column6.Name = "Column6";
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "Column7";
+            this.Column7.Name = "Column7";
+            // 
+            // Column8
+            // 
+            this.Column8.HeaderText = "Column8";
+            this.Column8.Name = "Column8";
+            // 
+            // Column9
+            // 
+            this.Column9.HeaderText = "Column9";
+            this.Column9.Name = "Column9";
+            // 
+            // Column10
+            // 
+            this.Column10.HeaderText = "Column10";
+            this.Column10.Name = "Column10";
+            // 
+            // Column11
+            // 
+            this.Column11.HeaderText = "Column11";
+            this.Column11.Name = "Column11";
+            // 
+            // Column12
+            // 
+            this.Column12.HeaderText = "Column12";
+            this.Column12.Name = "Column12";
+            // 
+            // Column13
+            // 
+            this.Column13.HeaderText = "Column13";
+            this.Column13.Name = "Column13";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(773, 568);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column9;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column10;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column11;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column12;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column13;
+    }
+}
+
---
+++ dotNet/desktop/JsonSample/1/1/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Collections;
+
+
+namespace _1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static readonly DateTime UnixEpoch = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+        static readonly double MaxUnixSeconds = (DateTime.MaxValue - UnixEpoch).TotalSeconds;
+
+        public static DateTime UnixTimeStampToDateTime2(double unixTimeStamp)
+        {
+            return unixTimeStamp > MaxUnixSeconds
+               ? UnixEpoch.AddMilliseconds(unixTimeStamp)
+               : UnixEpoch.AddSeconds(unixTimeStamp);
+        }
+
+        public static DateTime UnixTimeStampToDateTime(double unixTimeStamp)
+        {
+            // Unix timestamp is seconds past epoch
+            System.DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+            dtDateTime = dtDateTime.AddSeconds(unixTimeStamp).ToLocalTime();
+            return dtDateTime;
+        }
+
+        public static double DateTimeToUnixTimestamp(DateTime dateTime)
+        {
+            return (dateTime - new DateTime(1970, 1, 1).ToLocalTime()).TotalSeconds;
+        }
+
+
+        //jjose
+        public static double DateTimeToUnixTimestamp3(DateTime dateTime)
+        {
+            DateTime UnixEpoch = new DateTime(1970, 1, 1, 0, 0, 0);
+            UnixEpoch = UnixEpoch.ToLocalTime();
+
+            return (dateTime - UnixEpoch).TotalMilliseconds;
+        }
+
+        public static DateTime UnixTimeStampToDateTime3(double unixTimeStamp)
+        {
+            DateTime UnixEpoch = new DateTime(1970, 1, 1, 0, 0, 0);
+            UnixEpoch = UnixEpoch.ToLocalTime();
+            return UnixEpoch.AddMilliseconds(unixTimeStamp);
+        }
+        //jjose
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            WebClient vWClnt = new WebClient();
+            vWClnt.Proxy = WebRequest.DefaultWebProxy;
+            vWClnt.Proxy.Credentials = CredentialCache.DefaultCredentials;
+            //Get request
+            string vRetStr = vWClnt.DownloadString(textBox1.Text);
+
+            textBox2.Text = vRetStr;
+            //Extract it to hash table
+            Hashtable vHshtable = (Hashtable) Procurios.Public.JSON.JsonDecode(vRetStr);
+            //Get the features node
+            ArrayList vFeatItms = (ArrayList) vHshtable["features"];
+
+            DateTime  vUnxDtTime;
+            double vUnxDbl;
+            //for each feature
+            for (int i = 0; i < vFeatItms.Count ; i++)
+            {
+                Hashtable vFeature = (Hashtable)vFeatItms[i];
+                //get the attributes
+                Hashtable vAttrItms = (Hashtable)vFeature["attributes"];
+                //extract each attribute
+                string vObjId = (vAttrItms["OBJECTID"] == null) ? "" : vAttrItms["OBJECTID"].ToString();
+                string vMethaneReading = (vAttrItms["MethaneReading"] == null) ? "" : vAttrItms["MethaneReading"].ToString();
+                string vUniqueID = (vAttrItms["UniqueID"] == null) ? "" : vAttrItms["UniqueID"].ToString();
+                string vMisc = (vAttrItms["Misc"] == null) ? "" : vAttrItms["Misc"].ToString();
+                //string vEditDate = (vAttrItms["EditDate"] == null) ? "" : vAttrItms["EditDate"].ToString();
+                string vEditDate="";
+                if (!(vAttrItms["EditDate"] == null))
+                {
+                    double.TryParse(vAttrItms["EditDate"].ToString(), out vUnxDbl);
+                    vUnxDtTime = UnixTimeStampToDateTime2(vUnxDbl);
+                    vEditDate = vUnxDtTime.ToString("dd-mmm-yyyy"); 
+                }
+                string vEditor = (vAttrItms["Editor"] == null) ? "" : vAttrItms["Editor"].ToString();
+                string vCreationDate = (vAttrItms["CreationDate"] == null) ? "" : vAttrItms["CreationDate"].ToString();
+                string vDescription = (vAttrItms["Description"] == null) ? "" : vAttrItms["Description"].ToString();
+                string vObservation = (vAttrItms["Observation"] == null) ? "" : vAttrItms["Observation"].ToString();
+                string vCreator = (vAttrItms["Creator"] == null) ? "" : vAttrItms["Creator"].ToString();
+                string vGlobalId = (vAttrItms["GlobalID"] == null) ? "" : vAttrItms["GlobalID"].ToString();
+                //get the geometry
+                vAttrItms = (Hashtable)vFeature["geometry"];
+                //extract geometry
+                string vx = (vAttrItms["x"] == null) ? "" : vAttrItms["x"].ToString();
+                string vy = (vAttrItms["y"] == null) ? "" : vAttrItms["y"].ToString();
+
+                dataGridView1.Rows.Add(vObjId, vMethaneReading, vUniqueID, vMisc, vEditDate, vEditor, vCreationDate, vDescription, vObservation, vCreator, vGlobalId, vx, vy);
+
+                //double dtime = (double)LogRecord["time"];
+                //string source = (string)LogRecord["source"];
+                //string machine = (string)LogRecord["machine"];
+                //string user = (string)LogRecord["user"];
+                //double code = (double)LogRecord["code"];
+                //string elapsed = (string)LogRecord["elapsed"];
+                //string process = (string)LogRecord["process"];
+                //string thread = (string)LogRecord["thread"];
+                //string methodName = (string)LogRecord["methodName"];
+
+                //string scale = "NULL";
+
+                //string size_x = "NULL";
+                //string size_y = "NULL";
+
+                //string minx = "NULL";
+                //string miny = "NULL";
+                //string maxx = "NULL";
+                //string maxy = "NULL";
+
+                //string Shape = "NULL";
+
+                //DateTime dttime = UnixTimeStampToDateTime2(dtime);
+
+                //dttime = dttime.ToLocalTime();
+
+                //long lcode = (long)code;
+
+                //if (message.Length > 4000) message = message.Substring(0, 4000);
+                //if (methodName.Length > 50) methodName = methodName.Substring(0, 50);
+
+                //message = message.Replace("'", "''");
+
+                //if (message.Contains("Extent:"))
+                //{
+                //    string[] vals = message.Split(';');
+
+                //    string[] tmp_extent = vals[0].Split(':');
+                //    string[] tmp_size = vals[1].Split(':');
+                //    string[] tmp_scale = vals[2].Split(':');
+
+                //    string[] tmp_sizes = tmp_size[1].Split(',');
+                //    string[] tmp_extents = tmp_extent[1].Split(',');
+
+                //    scale = tmp_scale[1];
+
+                //    size_x = tmp_sizes[0];
+                //    size_y = tmp_sizes[1];
+
+                //    minx = tmp_extents[0];
+                //    miny = tmp_extents[1];
+                //    maxx = tmp_extents[2];
+                //    maxy = tmp_extents[3];
+
+                //    Shape = "'POLYGON((" + minx + " " + miny + "," + minx + " " + maxy + "," + maxx + " " + maxy + "," + maxx + " " + miny + "," + minx + " " + miny + "))'";
+
+                //    Shape = "geometry::STPolyFromText(" + Shape + ", " + srid + ")";
+                //}
+
+            }
+
+
+        }
+    }
+}
