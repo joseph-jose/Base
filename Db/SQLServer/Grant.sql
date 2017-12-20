2330a980c720aec0eb66621a00d406bdaa7173d9
joseph-jose <josephson.sw@gmail.com>
2017-12-21 11:48:21 +1300

    Add files via upload

---
+++ Db/SQLServer/Grant.sql
@@ -0,0 +1,10 @@
+Grant all on "GISWSL"."WA_PHLink" to GISEditor;
+Grant all on "GISWSL"."WA_PHLink_SEQ" to GISEditor;
+Grant all on "GISWSL"."WA_PWLink" to GISEditor;
+Grant all on "GISWSL"."WA_PWLink_SEQ" to GISEditor;
+Grant execute on "GISWSL"."NextVal" to GISEditor;
+Grant select on "GISWSL"."WA_PHLink" to GISViewer;
+Grant select on "GISWSL"."WA_PHLink_SEQ" to GISViewer;
+Grant select on "GISWSL"."WA_PWLink" to GISViewer;
+Grant select on "GISWSL"."WA_PWLink_SEQ" to GISViewer;
+Grant execute on "GISWSL"."NextVal" to GISViewer;
